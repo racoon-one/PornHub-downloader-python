6faf4d9f05b29f9a63c11570e2944e192bd7038f
racoon-one <71573664+racoon-one@users.noreply.github.com>
2021-08-08 01:04:25 +0200

    Create auto-install.sh
    created a script to automatically install the python script + dependencies

---
+++ auto-install.sh
@@ -0,0 +1,5 @@
+apt install python3 python3-pip unzip
+wget https://github.com/racoon-one/PornHub-downloader-python/archive/master.zip
+unzip master.zip
+cd PornHub-downloader-python-master
+pip3 install -r requirements.txt
