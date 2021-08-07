94113e89f3feb9edb5a9501888ae1c7d7da51980
racoon-one <71573664+racoon-one@users.noreply.github.com>
2021-08-08 01:19:45 +0200

    This was stupid so i deleted it again

--- auto-install.sh
+++
@@ -1,5 +0,0 @@
-apt install python3 python3-pip unzip
-wget https://github.com/racoon-one/PornHub-downloader-python/archive/master.zip
-unzip master.zip
-cd PornHub-downloader-python-master
-pip3 install -r requirements.txt
